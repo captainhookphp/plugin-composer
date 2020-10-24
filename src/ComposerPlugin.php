f03584770db072943a777f939c84b4ce51e0de1d
Andreas Heigl <a.heigl@bitexpert.de>
2020-10-24 18:08:48 +0200

    Update to Composer Plugin-API 2.0

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -41,6 +41,36 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
     }
 
     /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // Do nothing currently
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // Do nothing currently
+    }
+
+
+
+    /**
      * Make sure the installer is executed after the autoloader is created
      *
      * @return array
